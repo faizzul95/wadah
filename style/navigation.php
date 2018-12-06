d7cc09efba4c1a45d6840d165cf2a9fe6416e421
najwalotfi <45595647+najwalotfi@users.noreply.github.com>
2018-12-06 21:42:06 +0800

    update asset

--- style/navigation.php
+++ style/navigation.php
@@ -1,3 +1,7 @@
+<?php 
+// Turn off error reporting
+	error_reporting(0);
+ ?>
 			<div class="container">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
