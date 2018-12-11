082bc45a3ab718764358441919787cfe808bf578
najwalotfi <45595647+najwalotfi@users.noreply.github.com>
2018-12-11 16:44:19 +0800

    Update maintenance_list

--- asset/maintenance_list.php
+++ asset/maintenance_list.php
@@ -1,79 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Home</title>
-  <script type="text/javascript">
-	<?php include '../js/input_restriction.js';?>
-	<?php include '../connection.php';?>
-</script>
-</head>
-<body>
-        <?php
-                
-				$sql = "SELECT * FROM `maintenance` 
-				INNER JOIN `vendor` ON `vendor`.`vendor_id` = `maintenance`.`vendor_id` 
-				INNER JOIN `asset` ON `asset`.`asset_id` = `maintenance`.`asset_id`";
-				
-				
-$result = mysqli_query($myConnection, $sql) or die(mysqli_error($myConnection));
-                if (mysqli_num_rows($result)==0){
-                     echo "No result found";
-                  }else{
-          ?>
-            <table border="1">
-                <thead bgcolor="#57A0D2">
-                  <tr>
-                    <th scope="col"><center>#</center></th>
-                    <th scope="col"><center>Id Penyelenggaraan</center></th>
-                    <th scope="col"><center>Nama Penyelenggara</center></th>
-                    <th scope="col"><center>Tahap Penyelenggaraan</center></th>
-                    <th scope="col"><center>Kos Penyelenggaraan</center></th>
-                    <th scope="col"><center>Tempat Penyelenggaraan</center></th>
-                    <th scope="col"><center>Tempoh Hari Penyelenggaraan</center></th>
-                    <th colspan="2" scope="col"><center>Penerangan Penyelenggaraan</center></th>
-                  </tr>
-                </thead>
-                <tbody>
-                                         
-                  <?php
-                $count = 1;
-                while($row = mysqli_fetch_assoc($result))
-                 {  
-                  
-                  ?>
-                      <tr>
-                        <th scope="row"><center><?php echo $count; ?></center></th>
-                        <td><center><?php echo $row['maintenance_id']; ?></center></td>
-                        <td><center><?php echo $row['vendor_name']; ?></center></td>
-                        <td><center><?php echo $row['maintenance_status']; ?></center></td>
-                        <td><center><?php echo $row['maintenance_cost']; ?></center></td>
-                        <td><center><?php echo $row['asset_place']; ?></center></td>
-                        <td><center><?php echo $row['maintenance_days']; ?></center></td>
-                        <td><center><?php echo $row['maintenance_desc']; ?></center></td>
-                        <td><center>
-                            <button onclick="location.href='asset_update.php?edu_id=<?php echo $row['asset_id']; ?>';">Update</button>
-                            <form method="post" action="../asset/controller.php">
-                              <input type="hidden" name="delete_id" value="<?php echo $row['asset_id']; ?>">
-                              <input type="submit" name="delete_asset" value="Delete">
-                            </form>
-                          </center></td>
-                      </tr>
-                    <?php
-                    $count++;
-                  }
-                  ?>
-
-                <?php } ?>
-
-                  </tbody>
-                </table>
-
-</body>
-</html>
-
-
-
-
 <?php 
 
 require ('../connection.php');
@@ -219,13 +143,14 @@ $result = mysqli_query($myConnection, $sql) or die(mysqli_error($myConnection));
 		<footer class="footer">
 			<?php include '../style/footer.php'; ?>
 		</footer>
-
-		<!-- Bootstrap core JavaScript
+        		<!-- Bootstrap core JavaScript
 			================================================== -->
 		<!-- Placed at the end of the document so the pages load faster -->
+       
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="../js/bootstrap.min.js"></script>
 		<script src="../js/jquery.bxslider.js"></script>
 		<script src="../js/mooz.scripts.min.js"></script>
+        
 	</body>
 </html>
